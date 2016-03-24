d385f9b397c20ab987e644b21f5bffbb146c0d71
Lithir <uding@bradleys-mbp.dhcp.umsl.edu>
2016-03-23 21:32:43 -0500

    This is a test model, check it out menz.

---
+++ YouthExploringScience/MyModel.swift
@@ -0,0 +1,9 @@
+//
+//  MyModel.swift
+//  YouthExploringScience
+//
+//  Created by Bradley on 3/23/16.
+//  Copyright © 2016 St. Louis Science Center. All rights reserved.
+//
+
+import Foundation
